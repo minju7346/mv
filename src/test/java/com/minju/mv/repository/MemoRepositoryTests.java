19785110c1a754da7a533ec431c96788e34b3f10
minju7346 <minju7346@naver.com>
2022-06-30 23:04:07 +0900

    Initial commit

---
+++ src/test/java/com/minju/mv/repository/MemoRepositoryTests.java
@@ -0,0 +1,101 @@
+package com.minju.mv.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.minju.mv.entity.Memo;
+
+@SpringBootTest
+public class MemoRepositoryTests {
+
+	@Autowired
+	MemoRepository memoRepository;
+
+	@Test
+	public void testInsertDummies() {
+		IntStream.rangeClosed(1, 100).forEach(i -> {
+			Memo memo = Memo.builder().memoText("Sample..."+i).build();
+			memoRepository.save(memo);
+		});
+	}
+
+	@Test
+	public void testSelect(){
+
+		Long mno = 100L;
+
+		Optional<Memo> result = memoRepository.findById(mno);
+
+		System.out.println("----------------------");
+
+		if(result.isPresent()){
+			Memo memo = result.get();
+			System.out.println(memo);
+		}
+	}
+
+	@Test
+	public void testUpdate(){
+		Memo memo = Memo.builder().mno(100L).memoText("Update Text").build();
+
+		System.out.println(memoRepository.save(memo));
+	}
+
+	@Test
+	public void testDelete() {
+		Long mno = 100L;
+
+		memoRepository.deleteById(mno);
+	}
+
+	@Test
+	public void testPageDefault() {
+
+		Sort sort1 = Sort.by("mno").descending();
+
+		Pageable pageable = PageRequest.of(0,10,sort1);
+
+		Page<Memo> result = memoRepository.findAll(pageable);
+
+
+		result.get().forEach(memo -> {
+			System.out.println(memo);
+		});
+	}
+
+	@Test
+	public void testQueryMethods(){
+		List<Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(70L, 80L);
+
+		for (Memo memo : list) {
+			System.out.println(memo);
+		}
+	}
+
+	@Test
+	public void testQueryMethodWithPagable() {
+		Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+
+		Page<Memo> result = memoRepository.findByMnoBetween(10L, 50L, pageable);
+
+		result.get().forEach(memo -> System.out.println(memo));
+	}
+
+	@Commit // 최종결과를 커밋하기위해
+	@Transactional//selet문으로 해당 엔티티를 가져오고 삭제하는 작업이 같이 일어나야하기 때문에
+	@Test
+	public void testDeleteQueryMethods() {
+		memoRepository.deleteMemoByMnoLessThan(12L);
+	}
+}
